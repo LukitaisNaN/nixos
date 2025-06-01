733fd26afc7e7bc127e599d6ec172b445c2630e7
Lukita <luca.irrazabal@mi.unc.edu.ar>
2025-06-01 14:30:56 -0300

    added comments

--- hyprland.nix
+++ hyprland.nix
@@ -4,16 +4,17 @@
 
   environment.systemPackages = with pkgs; [
     brightnessctl
-    dunst
-    grim
-    hyprprop
-    hyprshot
-    nwg-look
-    playerctl # key ctrls
-    slurp
-    waybar
-    wl-clipboard
-    wofi
+    dunst             # Notification manager
+    grim              # hyprshot dependancy
+    hyprprop          # See which properties a window has 
+    hyprpolkitagent   # Popup that appears when an app wants elevated permissions
+    hyprshot          # Screenshots
+    nwg-look          # GTK-settings editor
+    playerctl         # For video ctrls
+    slurp             # hyprshot dependancy
+    waybar            # Wayland bar
+    wl-clipboard      # Copy/paste utilities
+    wofi              # Application launcher
 
  ];
 }
