aaca004fbdef3ae4992072888d1dea7d52e52134
Lukita <luca.irrazabal@mi.unc.edu.ar>
2025-06-19 13:01:52 -0300

    feat: hyprcursor

--- hyprland.nix
+++ hyprland.nix
@@ -2,19 +2,21 @@
 
 {
 
+  services.greetd.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
+
   environment.systemPackages = with pkgs; [
-    brightnessctl
+    brightnessctl     # Self-explanatory
     dunst             # Notification manager
-    grim              # hyprshot dependancy
+    grim slurp        # hyprshot dependancies
+    hyprcursor        # cursors
     hyprprop          # See which properties a window has 
     hyprpolkitagent   # Popup that appears when an app wants elevated permissions
     hyprshot          # Screenshots
     nwg-look          # GTK-settings editor
     playerctl         # For video ctrls
-    slurp             # hyprshot dependancy
     waybar            # Wayland bar
     wl-clipboard      # Copy/paste utilities
     wofi              # Application launcher
-
  ];
 }
