aabbdcf04d836422259ed69fbdf87610f30590b4
Lukita <luca.irrazabal@mi.unc.edu.ar>
2025-06-26 20:38:57 -0300

    feat: added initial passwords to users

--- users/luki/luki.nix
+++ users/luki/luki.nix
@@ -1,12 +1,31 @@
-{ config }:
+{ config, inputs, pkgs, system, ... }:
 {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
   users.users.lukita = {
     isNormalUser = true;
     description = "Lukita";
     extraGroups = [ "networkmanager" "wheel" ];
+    initialPassword = "1212";
     packages = with pkgs; [
       home-manager
+      lmms
     ];
   };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      lukita = import ./home.nix;
+    };
+  };
+
+  # System language.
+  i18n.defaultLocale = "en_US.UTF-8";
 
 }
