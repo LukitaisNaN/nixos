1ed65924f71bd29e2ea2e46f4145512b734a6f1a
Lukita <luca.irrazabal@mi.unc.edu.ar>
2025-06-26 21:37:28 -0300

    feat: build cinnamon with its default apps

--- desk-env/cinnamon.nix
+++ desk-env/cinnamon.nix
@@ -2,16 +2,17 @@
 
 {
 
+  services.libinput.enable = true;
 
-services.libinput.enable = true;
-services.displayManager.defaultSession = "cinnamon";
-services.xserver = {
-		enable = true;
-		displayManager.lightdm.enable = true;
-		desktopManager = {
-			cinnamon.enable = true;
-		};
-};
+  services.displayManager.defaultSession = "cinnamon";
+  services.cinnamon.apps.enable = true;
+  services.xserver = {
+                  enable = true;
+                  displayManager.lightdm.enable = true;
+                  desktopManager = {
+                          cinnamon.enable = true;
+                  };
+  };
 
 }
 
