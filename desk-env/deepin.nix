a831a76b21f01a30dfa823ac50430c3e31b471ef
Lukita <luca.irrazabal@mi.unc.edu.ar>
2025-07-31 18:42:29 -0300

    FIX

--- desk-env/deepin.nix
+++ desk-env/deepin.nix
@@ -5,7 +5,6 @@
   services.libinput.enable = true;
 
   #services.displayManager.defaultSession = "lightdm";
-  services.cinnamon.apps.enable = true;
   services.xserver = {
                   enable = true;
                   displayManager.lightdm.enable = true;
