95c707693f5ccfbe8af6e1c3483524a79482198b
Lukita <luca.irrazabal@mi.unc.edu.ar>
2025-08-07 02:32:30 -0300

    FIX: Fixed start.sh to use sh instead of bash

--- start.sh
+++ start.sh
@@ -1,46 +1,49 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Checking if nix configuration folder exists..."
 
-if [ ! -d "~/.config/nixos"]; then
+if [ ! -d "$HOME/.config/nixos" ]; then
   echo "Creating nix configuration directory..."
-  mkdir -p ~/.config/
-  cd ~/.config/
+  mkdir -p "$HOME/.config"
+  cd "$HOME/.config"
   echo "Downloading github repository..."
   git clone git@github.com:LukitaisNaN/nixos.git
   echo "OK!"
 else
-  echo "Directory already exists, skiping clonation..."
-  cd ~/.config/nixos
+  echo "Directory already exists, skipping clonation..."
+  cd "$HOME/.config/nixos"
   echo "Setting ssh url..."
 
-  # Set
   git remote set-url origin git@github.com:LukitaisNaN/nixos.git
 
-  # Check 
   if [ "$(git remote get-url origin)" = "git@github.com:LukitaisNaN/nixos.git" ]; then
     echo "OK!"
   else
     echo "Failed to verify remote URL. =("
     exit 1
   fi
-
 fi
 
-cd
+cd "$HOME"
 
-if [ -d "~/Desktop"]; then
-  ln -s ~/.config/nixos/users/ebo/home.nix ~/Desktop/apps.nix
+if [ -d "$HOME/Desktop" ]; then
+  ln -s "$HOME/.config/nixos/users/ebo/home.nix" "$HOME/Desktop/apps.nix"
 else
-  ln -s ~/.config/nixos/users/ebo/home.nix ~/Escritorio/apps.nix
+  ln -s "$HOME/.config/nixos/users/ebo/home.nix" "$HOME/Escritorio/apps.nix"
 fi
 
-sudo nixos-rebuild switch --flake ~/.config/nixos/#eborito
+sudo nixos-rebuild switch --flake "$HOME/.config/nixos/#eborito"
 
-cat "Ahora se va a generar una clave que es necesaria para la nube.
+echo "Ahora se va a generar una clave que es necesaria para la nube.
 Te va a pedir un par de cosas. No hace falta que escribas nada, solo tocá enter."  
 
 ssh-keygen
 
 echo "Ahora pasame lo de acá abajo por wsp :P"
-cat ~/.ssh/id*.pub
+
+if ls "$HOME/.ssh/id"*.pub 1> /dev/null 2>&1; then
+  cat "$HOME/.ssh/id"*.pub
+else
+  echo "No SSH public key found!"
+fi
+
