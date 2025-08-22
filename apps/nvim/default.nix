a5f5e297d3b7ef1d3bfff5218dd0ef2e338d78c6
Lukita <luca.irrazabal@mi.unc.edu.ar>
2025-08-22 16:05:43 -0300

    feat: Using nvim nightly instead of main branch

--- apps/nvim/default.nix
+++ apps/nvim/default.nix
@@ -1,12 +1,10 @@
 { pkgs, inputs, config, ... }:
 {
-programs.neovim = {
-  enable   = true;
-  vimAlias = true;
-};
 
-environment.systemPackages = with pkgs; [
-    vimPlugins.vim-plug
-];
+  programs.neovim = {
+    vimAlias = true;
+    enable   = true;
+    package  = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+  };
 
 }
